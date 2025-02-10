00a8d29dd9b1a49664aa8ddea56cad0bd1a71e76
Justus Magin <keewis@posteo.de>
2025-02-10 16:20:16 +0100

    switch to a mixed project

---
+++ python/healpix_geo/__init__.py
@@ -0,0 +1,6 @@
+from .healpix_geo import *
+
+
+__doc__ = healpix_geo.__doc__
+if hasattr(healpix_geo, "__all__"):
+    __all__ = healpix_geo.__all__
---
+++ python/tests/test_all.py
@@ -0,0 +1,6 @@
+import pytest
+import healpix_geo
+
+
+def test_sum_as_string():
+    assert healpix_geo.sum_as_string(1, 1) == "2"
