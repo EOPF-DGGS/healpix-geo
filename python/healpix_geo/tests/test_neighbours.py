c6908172697ff79b67bef7014da62798d288fd26
Justus Magin <keewis@posteo.de>
2025-02-10 19:53:22 +0100

    properly dispatch based on the indexing scheme

--- python/healpix_geo/tests/test_neighbours.py
+++ python/healpix_geo/tests/test_neighbours.py
@@ -2,18 +2,29 @@ import cdshealpix
 import numpy as np
 import pytest
 
-from healpix_geo import neighbours_in_kth_ring
+from healpix_geo import nested
 
 
 @pytest.mark.parametrize("depth", [2, 8])
 @pytest.mark.parametrize("ring", [0, 1])
-def test_neighbours_in_kth_ring(depth, ring):
+@pytest.mark.parametrize(
+    "indexing_scheme",
+    [
+        "nested",
+        pytest.param("ring", mark=pytest.mark.skip(reason="not implemented yet")),
+    ],
+)
+def test_neighbours_in_kth_ring(depth, ring, indexing_scheme):
+    if indexing_scheme == "nested":
+        neighbours_in_kth_ring = nested.neighbours_in_kth_ring
+        neighbours = cdshealpix.nested.neighbours
+
     ipixels = np.array([50, 100], dtype="int64")
 
     actual = neighbours_in_kth_ring(depth=depth, ipix=ipixels, ring=ring)
     if ring == 0:
         expected = np.reshape(ipixels, (-1, 1))
     else:
-        expected = cdshealpix.nested.neighbours(ipix=ipixels, depth=depth)
+        expected = neighbours(ipix=ipixels, depth=depth)
 
     np.testing.assert_equal(actual, expected)
