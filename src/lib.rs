014ee3ccaa4aa313bd2e6a7b1bc5fc6481c409fc
Justus Magin <keewis@posteo.de>
2025-02-18 20:06:03 +0100

    attempt to build a "zoom" function
    (won't compile because relevant methods are internal)

--- src/lib.rs
+++ src/lib.rs
@@ -53,6 +53,58 @@ mod nested {
         }
         Ok(())
     }
+
+    fn zoom_to<'a>(
+        _py: Python,
+        depth: u8,
+        ipix: &Bound<'a, PyArrayDyn<u64>>,
+        new_depth: u8,
+        result: &Bound<'a, PyArrayDyn<u64>>,
+        nthreads: u16,
+    ) -> PyResult<()> {
+        use cdshealpix::nested::HashParts;
+        let ipix = ipix.as_array();
+        let mut result = unsafe { result.as_array_mut() };
+        let layer = healpix::nested::get(depth);
+        let delta_depth: i16 = new_depth as i16 - depth as i16;
+
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            match delta_depth {
+                0 => {
+                    // TODO: copy cell ids
+                }
+                delta_depth if delta_depth < 0 => {
+                    // TODO: compute parent cell ids
+                    let pool = rayon::ThreadPoolBuilder::new()
+                        .num_threads(nthreads as usize)
+                        .build()
+                        .unwrap();
+                    pool.install(|| {
+                        Zip::from(result.rows_mut())
+                            .and(&ipix)
+                            .par_for_each(|mut n, &p| {
+                                HashParts { d0h, i, j } = layer.decode_hash(p);
+
+                                let shift = -delta_depth;
+
+                                let i = i >> shift;
+                                let j = j >> shift;
+
+                                let parent = layer.build_hash_from_parts(d0h, i, j);
+
+                                n[0] = parent;
+                            })
+                    });
+                }
+                delta_depth if delta_depth > 0 => {
+                    // TODO: compute child cell ids. Make sure `result` has enough space.
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
 
 #[pymodule]
