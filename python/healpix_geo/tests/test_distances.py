2b9e2c1448360a1e9e0a8d00e5260df53f16dba6
Justus Magin <keewis@posteo.de>
2025-02-18 12:19:53 +0100

    test the computation of angular distances

---
+++ python/healpix_geo/tests/test_distances.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pytest
+
+import healpix_geo
+
+
+@pytest.mark.parametrize(
+    ["depth", "indexing_scheme", "from_", "to_", "expected"],
+    (
+        (
+            1,
+            "nested",
+            np.array([0, 16, 25, 32, 46]),
+            np.array([[2], [15], [27], [40], [41]], dtype="int64"),
+            np.array(
+                [[0.51262797], [1.60992678], [0.51347673], [0.82227572], [0.57850402]],
+                dtype="float64",
+            ),
+        ),
+        (
+            2,
+            "ring",
+            np.array([0, 16, 25, 32, 46]),
+            np.array([[2, 4], [15, 7], [27, 26], [40, -1], [-1, 41]], dtype="int64"),
+            np.array(
+                [
+                    [0.4089604, 0.23486912],
+                    [0.30291976, 0.283655],
+                    [0.57850402, 0.29185825],
+                    [1.87523829, np.nan],
+                    [np.nan, 1.60781736],
+                ],
+                dtype="float64",
+            ),
+        ),
+    ),
+)
+def test_distance(depth, indexing_scheme, from_, to_, expected):
+    if indexing_scheme == "nested":
+        angular_distances = healpix_geo.nested.angular_distances
+    elif indexing_scheme == "ring":
+        angular_distances = healpix_geo.ring.angular_distances
+
+    actual = angular_distances(from_, to_, depth)
+
+    np.testing.assert_allclose(actual, expected)
