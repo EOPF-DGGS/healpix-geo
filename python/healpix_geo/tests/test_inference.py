920d41cb4cafe5a8611e78fcd6ad332848d5c50d
Justus Magin <keewis@posteo.de>
2025-05-21 15:24:40 +0200

    check that the vertices are consistent for spherical

--- python/healpix_geo/tests/test_inference.py
+++ python/healpix_geo/tests/test_inference.py
@@ -123,3 +123,36 @@ class TestGeographicToHealpix:
         expected = cds_lonlat_to_healpix(lon_, lat_, param_cds)
 
         np.testing.assert_equal(actual, expected)
+
+
+class TestVertices:
+    @pytest.mark.parametrize(
+        ["cell_ids", "depth", "indexing_scheme"],
+        (
+            pytest.param(np.array([0, 4, 5, 7, 9]), 0, "ring", id="level0-ring"),
+            pytest.param(np.array([1, 2, 3, 8]), 0, "nested", id="level0-nested"),
+            pytest.param(
+                np.array([3, 19, 54, 63, 104, 127]), 4, "ring", id="level4-ring"
+            ),
+            pytest.param(
+                np.array([22, 89, 134, 154, 190]), 4, "nested", id="level4-nested"
+            ),
+        ),
+    )
+    def test_spherical(self, cell_ids, depth, indexing_scheme):
+        if indexing_scheme == "ring":
+            param_cds = 2**depth
+            hg_vertices = healpix_geo.ring.vertices
+            cds_vertices = cdshealpix.ring.vertices
+        else:
+            param_cds = depth
+            hg_vertices = healpix_geo.nested.vertices
+            cds_vertices = cdshealpix.nested.vertices
+
+        actual_lon, actual_lat = hg_vertices(cell_ids, depth, ellipsoid="sphere")
+        expected_lon_, expected_lat_ = cds_vertices(cell_ids, param_cds)
+        expected_lon = np.asarray(expected_lon_.to("degree"))
+        expected_lat = np.asarray(expected_lat_.to("degree"))
+
+        np.testing.assert_allclose(actual_lon, expected_lon)
+        np.testing.assert_allclose(actual_lat, expected_lat)
