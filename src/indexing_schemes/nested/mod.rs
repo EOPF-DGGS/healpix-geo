7995cf7bbf1632663261f617d9a2fff1ff59db8d
Justus Magin <keewis@posteo.de>
2025-11-04 16:58:37 +0100

    use `self` to refer to sub-crates

--- src/indexing_schemes/nested/mod.rs
+++ src/indexing_schemes/nested/mod.rs
@@ -1,5 +1,7 @@
 mod coordinates;
 mod hierarchy;
 
-pub(crate) use coordinates::{angular_distances, healpix_to_lonlat, lonlat_to_healpix, vertices};
-pub(crate) use hierarchy::{kth_neighbourhood, siblings, zoom_to};
+pub(crate) use self::coordinates::{
+    angular_distances, healpix_to_lonlat, lonlat_to_healpix, vertices,
+};
+pub(crate) use self::hierarchy::{kth_neighbourhood, siblings, zoom_to};
