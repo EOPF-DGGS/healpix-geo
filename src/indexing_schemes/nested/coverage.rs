b2d1b4e6477dd09e3aa280a2c28a47b055193135
Justus Magin <keewis@posteo.de>
2025-11-25 15:49:34 +0100

    use `EllipsoidLike` in the coverage functions

--- src/indexing_schemes/nested/coverage.rs
+++ src/indexing_schemes/nested/coverage.rs
@@ -1,5 +1,5 @@
+use crate::ellipsoid::{EllipsoidLike, IntoGeodesyEllipsoid};
 use cdshealpix as healpix;
-use geodesy::Ellipsoid;
 use ndarray::Array1;
 use numpy::{IntoPyArray, PyArray1, PyArray2, PyArrayMethods};
 use pyo3::exceptions::PyValueError;
@@ -45,20 +45,21 @@ fn get_flat_cells(bmoc: healpix::nested::bmoc::BMOC) -> (Array1<u64>, Array1<u8>
 
 #[allow(clippy::type_complexity)]
 #[pyfunction]
-#[pyo3(signature = (depth, bbox, *, ellipsoid = "sphere", flat = true))]
+#[pyo3(signature = (depth, bbox, *, ellipsoid, flat = true))]
 pub(crate) fn zone_coverage<'py>(
     py: Python<'py>,
     depth: u8,
     bbox: (f64, f64, f64, f64),
-    ellipsoid: &str,
+    ellipsoid: EllipsoidLike,
     flat: bool,
 ) -> PyResult<(
     Bound<'py, PyArray1<u64>>,
     Bound<'py, PyArray1<u8>>,
     Bound<'py, PyArray1<bool>>,
 )> {
-    let ellipsoid_ =
-        Ellipsoid::named(ellipsoid).map_err(|e| PyValueError::new_err(e.to_string()))?;
+    let is_spherical = ellipsoid.is_spherical();
+    let ellipsoid_ = ellipsoid.into_geodesy_ellipsoid()?;
+
     let coefficients = ellipsoid_.coefficients_for_authalic_latitude_computations();
 
     let (lon_min, lat_min, lon_max, lat_max) = bbox;
@@ -66,9 +67,17 @@ pub(crate) fn zone_coverage<'py>(
     let layer = healpix::nested::get(depth);
     let bmoc = layer.zone_coverage(
         lon_min.rem_euclid(360.0).to_radians(),
-        ellipsoid_.latitude_geographic_to_authalic(lat_min.to_radians(), &coefficients),
+        if is_spherical {
+            lat_min.to_radians()
+        } else {
+            ellipsoid_.latitude_geographic_to_authalic(lat_min.to_radians(), &coefficients)
+        },
         lon_max.rem_euclid(360.0).to_radians(),
-        ellipsoid_.latitude_geographic_to_authalic(lat_max.to_radians(), &coefficients),
+        if is_spherical {
+            lat_max.to_radians()
+        } else {
+            ellipsoid_.latitude_geographic_to_authalic(lat_max.to_radians(), &coefficients)
+        },
     );
 
     let (ipix, moc_depth, fully_covered) = if flat {
@@ -86,22 +95,23 @@ pub(crate) fn zone_coverage<'py>(
 
 #[allow(clippy::type_complexity)]
 #[pyfunction]
-#[pyo3(signature = (depth, center, size, angle, *, ellipsoid = "sphere", flat = true))]
+#[pyo3(signature = (depth, center, size, angle, *, ellipsoid, flat = true))]
 pub(crate) fn box_coverage<'py>(
     py: Python<'py>,
     depth: u8,
     center: (f64, f64),
     size: (f64, f64),
     angle: f64,
-    ellipsoid: &str,
+    ellipsoid: EllipsoidLike,
     flat: bool,
 ) -> PyResult<(
     Bound<'py, PyArray1<u64>>,
     Bound<'py, PyArray1<u8>>,
     Bound<'py, PyArray1<bool>>,
 )> {
-    let ellipsoid_ =
-        Ellipsoid::named(ellipsoid).map_err(|e| PyValueError::new_err(e.to_string()))?;
+    let is_spherical = ellipsoid.is_spherical();
+    let ellipsoid_ = ellipsoid.into_geodesy_ellipsoid()?;
+
     let coefficients = ellipsoid_.coefficients_for_authalic_latitude_computations();
 
     let (lon, lat) = center;
@@ -110,7 +120,11 @@ pub(crate) fn box_coverage<'py>(
     let layer = healpix::nested::get(depth);
     let bmoc = layer.box_coverage(
         lon.rem_euclid(360.0).to_radians(),
-        ellipsoid_.latitude_geographic_to_authalic(lat.to_radians(), &coefficients),
+        if is_spherical {
+            lat.to_radians()
+        } else {
+            ellipsoid_.latitude_geographic_to_authalic(lat.to_radians(), &coefficients)
+        },
         size_lon.rem_euclid(360.0).to_radians(),
         size_lat.to_radians(),
         angle.to_radians(),
@@ -131,12 +145,12 @@ pub(crate) fn box_coverage<'py>(
 
 #[allow(clippy::type_complexity)]
 #[pyfunction]
-#[pyo3(signature = (depth, vertices, *, ellipsoid = "sphere", exact = false, flat = true))]
+#[pyo3(signature = (depth, vertices, *, ellipsoid, exact = false, flat = true))]
 pub(crate) fn polygon_coverage<'py>(
     py: Python<'py>,
     depth: u8,
     vertices: &Bound<PyArray2<f64>>,
-    ellipsoid: &str,
+    ellipsoid: EllipsoidLike,
     exact: bool,
     flat: bool,
 ) -> PyResult<(
@@ -146,8 +160,9 @@ pub(crate) fn polygon_coverage<'py>(
 )> {
     let vertices = unsafe { vertices.as_array() };
 
-    let ellipsoid_ =
-        Ellipsoid::named(ellipsoid).map_err(|e| PyValueError::new_err(e.to_string()))?;
+    let is_spherical = ellipsoid.is_spherical();
+    let ellipsoid_ = ellipsoid.into_geodesy_ellipsoid()?;
+
     let coefficients = ellipsoid_.coefficients_for_authalic_latitude_computations();
 
     let converted_vertices: Vec<(f64, f64)> = vertices
@@ -158,7 +173,11 @@ pub(crate) fn polygon_coverage<'py>(
             let lat = row[1];
             (
                 lon.rem_euclid(360.0).to_radians(),
-                ellipsoid_.latitude_geographic_to_authalic(lat.to_radians(), &coefficients),
+                if is_spherical {
+                    lat.to_radians()
+                } else {
+                    ellipsoid_.latitude_geographic_to_authalic(lat.to_radians(), &coefficients)
+                },
             )
         })
         .collect();
@@ -181,13 +200,13 @@ pub(crate) fn polygon_coverage<'py>(
 
 #[allow(clippy::type_complexity)]
 #[pyfunction]
-#[pyo3(signature = (depth, center, radius, *, ellipsoid = "sphere", delta_depth = 0, flat = true))]
+#[pyo3(signature = (depth, center, radius, *, ellipsoid, delta_depth = 0, flat = true))]
 pub(crate) fn cone_coverage<'py>(
     py: Python<'py>,
     depth: u8,
     center: (f64, f64),
     radius: f64,
-    ellipsoid: &str,
+    ellipsoid: EllipsoidLike,
     delta_depth: u8,
     flat: bool,
 ) -> PyResult<(
@@ -195,8 +214,9 @@ pub(crate) fn cone_coverage<'py>(
     Bound<'py, PyArray1<u8>>,
     Bound<'py, PyArray1<bool>>,
 )> {
-    let ellipsoid_ =
-        Ellipsoid::named(ellipsoid).map_err(|e| PyValueError::new_err(e.to_string()))?;
+    let is_spherical = ellipsoid.is_spherical();
+    let ellipsoid_ = ellipsoid.into_geodesy_ellipsoid()?;
+
     let coefficients = ellipsoid_.coefficients_for_authalic_latitude_computations();
 
     if depth > 29 {
@@ -215,7 +235,11 @@ pub(crate) fn cone_coverage<'py>(
     let bmoc = layer.cone_coverage_approx_custom(
         delta_depth,
         lon.rem_euclid(360.0).to_radians(),
-        ellipsoid_.latitude_geographic_to_authalic(lat.to_radians(), &coefficients),
+        if is_spherical {
+            lat.to_radians()
+        } else {
+            ellipsoid_.latitude_geographic_to_authalic(lat.to_radians(), &coefficients)
+        },
         radius.to_radians(),
     );
 
@@ -234,14 +258,14 @@ pub(crate) fn cone_coverage<'py>(
 
 #[allow(clippy::type_complexity, clippy::too_many_arguments)]
 #[pyfunction]
-#[pyo3(signature = (depth, center, ellipse_geometry, position_angle, *, ellipsoid = "sphere", delta_depth = 0, flat = true))]
+#[pyo3(signature = (depth, center, ellipse_geometry, position_angle, *, ellipsoid, delta_depth = 0, flat = true))]
 pub(crate) fn elliptical_cone_coverage<'py>(
     py: Python<'py>,
     depth: u8,
     center: (f64, f64),
     ellipse_geometry: (f64, f64),
     position_angle: f64,
-    ellipsoid: &str,
+    ellipsoid: EllipsoidLike,
     delta_depth: u8,
     flat: bool,
 ) -> PyResult<(
@@ -259,8 +283,9 @@ pub(crate) fn elliptical_cone_coverage<'py>(
         ));
     }
 
-    let ellipsoid_ =
-        Ellipsoid::named(ellipsoid).map_err(|e| PyValueError::new_err(e.to_string()))?;
+    let is_spherical = ellipsoid.is_spherical();
+    let ellipsoid_ = ellipsoid.into_geodesy_ellipsoid()?;
+
     let coefficients = ellipsoid_.coefficients_for_authalic_latitude_computations();
 
     let (lon, lat) = center;
@@ -270,7 +295,11 @@ pub(crate) fn elliptical_cone_coverage<'py>(
     let bmoc = layer.elliptical_cone_coverage_custom(
         delta_depth,
         lon.rem_euclid(360.0).to_radians(),
-        ellipsoid_.latitude_geographic_to_authalic(lat.to_radians(), &coefficients),
+        if is_spherical {
+            lat.to_radians()
+        } else {
+            ellipsoid_.latitude_geographic_to_authalic(lat.to_radians(), &coefficients)
+        },
         a.to_radians(),
         b.to_radians(),
         position_angle.to_radians(),
