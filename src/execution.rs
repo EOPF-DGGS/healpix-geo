f9cb2453c0dca3a4e8cf696e0e044899ef22a5e9
Justus Magin <keewis@posteo.de>
2025-11-26 11:55:33 +0100

    rename the macro parameters

--- src/execution.rs
+++ src/execution.rs
@@ -1,17 +1,17 @@
 #[macro_export]
 macro_rules! maybe_parallelize {
-    ($nthreads:ident, $first:expr, $second:expr $(,)?) => {
+    ($nthreads:ident, $iterable:expr, $func:expr $(,)?) => {
         #[cfg(not(target_arch = "wasm32"))]
         {
             let pool = rayon::ThreadPoolBuilder::new()
                 .num_threads($nthreads as usize)
                 .build()
                 .unwrap();
-            pool.install(|| $first.par_for_each($second));
+            pool.install(|| $iterable.par_for_each($func));
         }
         #[cfg(target_arch = "wasm32")]
         {
-            $first.for_each($second);
+            $iterable.for_each($func);
         }
     };
 }
