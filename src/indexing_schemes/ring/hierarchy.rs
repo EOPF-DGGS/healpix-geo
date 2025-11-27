4f2a11b471a88a661e43b02049460e41ca0d4a4d
Justus Magin <keewis@posteo.de>
2025-11-27 14:51:56 +0100

    deduplicate `ring.hierarchy` using the macro

--- src/indexing_schemes/ring/hierarchy.rs
+++ src/indexing_schemes/ring/hierarchy.rs
@@ -16,44 +16,24 @@ pub(crate) fn kth_neighbourhood<'a>(
 ) -> PyResult<()> {
     let ipix = unsafe { ipix.as_array() };
     let mut neighbours = unsafe { neighbours.as_array_mut() };
+
     let layer = healpix::nested::get(depth);
-    #[cfg(not(target_arch = "wasm32"))]
-    {
-        let pool = rayon::ThreadPoolBuilder::new()
-            .num_threads(nthreads as usize)
-            .build()
-            .unwrap();
-        pool.install(|| {
-            Zip::from(neighbours.rows_mut())
-                .and(&ipix)
-                .par_for_each(|mut n, &p| {
-                    let p_nested = layer.from_ring(p);
-                    let map = Array1::from_iter(
-                        layer
-                            .kth_neighbourhood(p_nested, ring)
-                            .into_iter()
-                            .map(|v| layer.to_ring(v) as i64),
-                    );
 
-                    n.slice_mut(s![..map.len()]).assign(&map);
-                })
-        });
-    }
-    #[cfg(target_arch = "wasm32")]
-    {
-        Zip::from(neighbours.rows_mut())
-            .and(&ipix)
-            .for_each(|mut n, &p| {
-                let p_nested = layer.from_ring(p);
-                let map = Array1::from_iter(
-                    layer
-                        .kth_neighbourhood(p_nested, ring)
-                        .into_iter()
-                        .map(|v| layer.to_ring(v) as i64),
-                );
+    maybe_parallelize!(
+        nthreads,
+        Zip::from(neighbours.rows_mut()).and(&ipix),
+        |mut n, &p| {
+            let p_nested = layer.from_ring(p);
+            let map = Array1::from_iter(
+                layer
+                    .kth_neighbourhood(p_nested, ring)
+                    .into_iter()
+                    .map(|v| layer.to_ring(v) as i64),
+            );
 
-                n.slice_mut(s![..]).assign(&map);
-            });
-    }
+            n.slice_mut(s![..map.len()]).assign(&map);
+        },
+    );
+
     Ok(())
 }
