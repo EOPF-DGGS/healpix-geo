bfa12223d4bdacca3ecd0bcf1333c63ec8f2e411
Justus Magin <keewis@posteo.de>
2026-02-19 14:21:46 +0100

    rewrite the environment templating

--- docs/create_environment.py
+++ docs/create_environment.py
@@ -6,13 +6,18 @@ import pathlib
 def main():
     root = pathlib.Path(__file__).parent
 
-    local_channel = f"file://{root.parent / 'target/conda'}"
+    local_channel = root.parent / "target/conda"
+    local_packages = list(local_channel.glob("emscripten-wasm32/healpix-geo*.conda"))
+    if len(local_packages) != 1:
+        raise RuntimeError(f"zero or more than one package found: {local_packages}")
+
+    local_package = local_packages[0]
 
     template_path = root / "environment_template.yml"
     env_path = root / "environment.yml"
 
     template = template_path.read_text()
-    environment = template.replace("{{ local-channel }}", local_channel)
+    environment = template.replace('"{{ local-package }}"', local_package)
 
     env_path.write_text(environment)
 
