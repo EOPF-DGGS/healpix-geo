79e6ad33aa5585407e7939a37b7d0bba14bbff2d
Justus Magin <keewis@posteo.de>
2026-03-18 17:36:53 +0100

    `vertices` and `kth_neighbourhood`

--- python/healpix_geo/auto.py
+++ python/healpix_geo/auto.py
@@ -63,3 +63,21 @@ def lonlat_to_healpix(
     module = _dispatch_module(grid.indexing_scheme)
     params = grid._as_params()
     return module.lonlat_to_healpix(lon, lat, num_threads=num_threads, **params)
+
+
+def vertices(
+    ipix: npt.NDArray[np.uint64], grid: Grid, *, num_threads: int = 0
+) -> (npt.NDArray[np.float64], npt.NDArray[np.float64]):
+    module = _dispatch_module(grid.indexing_scheme)
+    params = grid._as_params()
+
+    return module.vertices(ipix, num_threads=num_threads, **params)
+
+
+def kth_neighbourhood(
+    ipix: npt.NDArray[np.uint64], grid: Grid, *, ring: int, num_threads: int = 0
+) -> npt.NDArray[np.int64]:
+    module = _dispatch_module(grid.indexing_scheme)
+    params = grid._as_params()
+
+    return module.kth_neighbourhood(ipix, ring=ring, num_threads=num_threads, **params)
