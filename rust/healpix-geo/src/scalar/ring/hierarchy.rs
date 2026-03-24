beb7ef1f9c72af1b1cea47cfad3125c6591701a6
Justus Magin <keewis@posteo.de>
2026-03-24 16:42:09 +0100

    resolve typing problems in ring::hierarchy

--- rust/healpix-geo/src/scalar/ring/hierarchy.rs
+++ rust/healpix-geo/src/scalar/ring/hierarchy.rs
@@ -1,15 +1,20 @@
 use cdshealpix as healpix;
 
 pub fn kth_neighbourhood(hash: &u64, nside: &u32, ring: &u32) -> Vec<i64> {
-    let depth = healpix::depth(nside);
-    let layer = healpix::nested::get(depth);
+    let layer = healpix::nested::get(healpix::depth(*nside));
 
-    let hash_nested = layer.from_ring(hash);
+    let hash_nested = layer.from_ring(*hash);
 
     layer
         .kth_neighbourhood(hash_nested, *ring)
         .into_iter()
         .map(|v| v as i64)
-        .map(|v| if v == -1 { v } else { layer.to_ring(v) })
+        .map(|v| {
+            if v == -1 {
+                v
+            } else {
+                layer.to_ring(v as u64) as i64
+            }
+        })
         .collect()
 }
