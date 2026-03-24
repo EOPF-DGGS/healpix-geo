401c229f60550f8f38b374e1a7a0e60802fb988d
Justus Magin <keewis@posteo.de>
2026-03-24 15:50:57 +0100

    more work on the coverage module

--- rust/healpix-geo/src/scalar/nested/coverage.rs
+++ rust/healpix-geo/src/scalar/nested/coverage.rs
@@ -41,9 +41,127 @@ fn get_flat_cells(bmoc: BMOC) -> (Vec<u64>, Vec<u8>, Vec<bool>) {
 
 pub fn zone_coverage(
     bbox: (f64, f64, f64, f64),
+    layer: &Layer,
     ellipsoid: &Ellipsoid,
-    is_spherical: &bool,
-    flat: &bool
+    flat: bool
 ) -> (Vec<u64>, Vec<u64>, Vec<u64>) {
+    let (lon_min, lat_min, lon_max, lat_max) = bbox;
+
+    let bmoc = layer.zone_coverage(
+        lon_min.rem_euclid(360.0).to_radians(),
+        ellipsoid.geographic_to_authalic_latitude(lat_min.radians()),
+        lon_max.rem_euclid(360.0).to_radians(),
+        ellipsoid.geographic_to_authalic_latitude(lat_max.radians()),
+    );
+
+    if flat {
+        get_flat_cells(bmoc)
+    } else {
+        get_cells(bmoc)
+    }
+}
+
+pub fn box_coverage(
+    center: (f64, f64),
+    size: (f64, f64),
+    angle: f64,
+    layer: &Layer,
+    ellipsoid: &Ellipsoid,
+    flat: bool,
+) -> (Vec<u64>, Vec<u64>, Vec<u64>) {
+    let (lon, lat) = center;
+    let (size_lon, size_lat) = size;
+
+    let bmoc = layer.box_coverage(
+        lon.rem_euclid(360.0).to_radians(),
+        ellipsoid.geographic_to_authalic_latitude(lat.to_radians()),
+        size_lon.rem_euclid(360.0).to_radians(),
+        size_lat.to_radians(),
+        angle.to_radians(),
+    );
+
+    if flat {
+        get_flat_cells(bmoc)
+    } else {
+        get_cells(bmoc)
+    }
+}
+
+pub fn polygon_coverage(vertices: &[(f64, f64)], layer: &Layer, ellipsoid: &Ellipsoid, exact: bool, flat: bool) -> (Vec<u64>, Vec<u64>, Vec<u64>) {
+    let converted_vertices: Vec<(f64, f64)> = vertices.iter().map(|v| {
+        let (lon, lat) = v;
+
+        (lon.rem_euclid(360.0).to_radians(), ellipsoid.geographic_to_authalic_latitude(lat.to_radians()))
+    }).collect();
+
+    let bmoc = layer.polygon_coverage(&converted_vertices, exact);
+
+    if flat {
+        get_flat_cells(bmoc)
+    } else {
+        get_cells(bmoc)
+    }
+}
+
+pub fn cone_coverage(
+    center: (f64, f64),
+    radius: f64,
+    layer: &Layer,
+    ellipsoid: &Ellipsoid,
+    delta_depth: u8,
+    flat: bool,
+) -> (Vec<u64>, Vec<u64>, Vec<u64>) {
+    if layer.depth + delta_depth > 29 {
+        // TODO: return a Result object
+        panic!("delta_depth must be chosen such that layer.depth + delta_depth <= 29");
+    }
+
+    let (lon, lat) = center;
+
+    let bmoc = layer.cone_coverage_approx_custom(
+        delta_depth,
+        lon.rem_euclid(360.0).to_radians(),
+        ellipsoid.geographic_to_authalic_latitude(lat.to_radians()),
+        radius.to_radians(),
+    );
+
+    if flat {
+        get_flat_cells(bmoc)
+    } else {
+        get_cells(bmoc)
+    }
+}
+
+pub fn elliptical_cone_coverage(
+    center: (f64, f64),
+    ellipse_geometry: (f64, f64),
+    position_angle: f64,
+    ellipsoid: &Ellipsoid,
+    delta_depth: u8,
+    flat: bool,
+) -> (Vec<u64>, Vec<u64>, Vec<u64>) {
+    if layer.depth + delta_depth > 29 {
+        // TODO: return a Result object
+        panic!("delta_depth must be chosen such that layer.depth + delta_depth <= 29");
+    }
+
+    let (lon, lat) = center;
+
+    let (a, b) = ellipse_geometry;
+
+    let bmoc = layer.elliptical_cone_coverage_custom(
+        delta_depth,
+        lon.rem_euclid(360.0).to_radians(),
+        ellipsoid.geographic_to_authalic_latitude(lat.to_radians()),
+        a.to_radians(),
+        b.to_radians(),
+        position_angle.to_radians(),
+    );
+
+    if flat {
+        get_flat_cells(bmoc)
+    } else {
+        get_cells(bmoc)
+    }
 
 }
