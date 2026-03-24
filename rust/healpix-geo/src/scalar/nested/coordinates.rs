4795d65971841fa5a191cc0a76f9deb2cc6fba65
Justus Magin <keewis@posteo.de>
2026-03-24 15:32:45 +0100

    use the new ellipsoid structure to simplify the API

--- rust/healpix-geo/src/scalar/nested/coordinates.rs
+++ rust/healpix-geo/src/scalar/nested/coordinates.rs
@@ -1,55 +1,27 @@
-use crate::ellipsoid::{EllipsoidLike, IntoGeodesyEllipsoid};
+use crate::ellipsoid::Ellipsoid;
+
 use cdshealpix as healpix;
 use cdshealpix::nested::Layer;
-use geodesy::authoring::FourierCoefficients;
-use geodesy::ellps::{Ellipsoid, Latitudes};
 
-pub fn healpix_to_lonlat(
-    hash: &u64,
-    layer: &Layer,
-    ellipsoid: &Ellipsoid,
-    coefficients: &FourierCoefficients,
-    is_spherical: &bool,
-) -> (f64, f64) {
+pub fn healpix_to_lonlat(hash: &u64, layer: &Layer, ellipsoid: &Ellipsoid) -> (f64, f64) {
     let center = layer.center(*hash);
-    let lon = center.0.to_degrees();
 
-    let lat = if *is_spherical {
-        center.1.to_degrees()
-    } else {
-        ellipsoid
-            .latitude_authalic_to_geographic(center.1, coefficients)
-            .to_degrees()
-    };
+    let lon = center.0.to_degrees();
+    let lat = ellipsoid
+        .authalic_to_geographic_latitude(center.1)
+        .to_degrees();
 
     (lon, lat)
 }
 
-pub fn lonlat_to_healpix(
-    lon: &f64,
-    lat: &f64,
-    layer: &Layer,
-    ellipsoid: &Ellipsoid,
-    coefficients: &FourierCoefficients,
-    is_spherical: &bool,
-) -> u64 {
+pub fn lonlat_to_healpix(lon: &f64, lat: &f64, layer: &Layer, ellipsoid: &Ellipsoid) -> u64 {
     let lon_ = lon.to_radians();
-    let lat_ = if *is_spherical {
-        lat.to_radians()
-    } else {
-        ellipsoid.latitude_geographic_to_authalic(lat.to_radians(), coefficients)
-    };
+    let lat_ = ellipsoid.geographic_to_authalic_latitude(lat.to_radians());
 
     layer.hash(lon_, lat_)
 }
 
-pub fn vertices(
-    hash: &u64,
-    layer: &Layer,
-    ellipsoid: &Ellipsoid,
-    coefficients: &FourierCoefficients,
-    is_spherical: &bool,
-) -> (Vec<f64>, Vec<f64>) {
+pub fn vertices(hash: &u64, layer: &Layer, ellipsoid: &Ellipsoid) -> (Vec<f64>, Vec<f64>) {
     let vertices = layer.vertices(*hash);
 
     let (vertex_lon, vertex_lat): (Vec<f64>, Vec<f64>) = vertices.into_iter().unzip();
@@ -58,18 +30,11 @@ pub fn vertices(
         .into_iter()
         .map(|l| l.to_degrees().rem_euclid(360.0))
         .collect());
-    let vertex_lat_: Vec<f64> = if *is_spherical {
-        vertex_lat.into_iter().map(|l| l.to_degrees()).collect()
-    } else {
-        vertex_lat
-            .into_iter()
-            .map(|l| {
-                ellipsoid
-                    .latitude_authalic_to_geographic(l, coefficients)
-                    .to_degrees()
-            })
-            .collect()
-    };
+    // FIXME: this will perform a lot more comparisons, figure out how to avoid that
+    let vertex_lat_: Vec<f64> = vertex_lat
+        .into_iter()
+        .map(|l| ellipsoid.authalic_to_geographic_latitude().to_degrees())
+        .collect();
 
     (vertex_lon_, vertex_lat_)
 }
